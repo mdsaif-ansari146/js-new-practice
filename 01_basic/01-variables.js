432f581e18b86aa7cc7bafe4902cda172b1c9f31
mdsaif-ansari146 <168331525+mdsaif-ansari146@users.noreply.github.com>
2025-01-04 05:38:46 +0000

    dataTypes and Variables

---
+++ 01_basic/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 122
+let accountEmail = "saif@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 3  // not allowed
+
+accountEmail = "hv@gmail.com"
+accountPassword = "000"
+accountCity = "DELHI"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+
+console.table([accountState,accountId,accountEmail,accountPassword, accountCity])
---
+++ 01_basic/02-dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" //treat all js code as newer version\
+
+// alert ("3+3") // we are using nodejs,not browser
+
+
+console.log(3
+    +
+    3
+);              // code readability should be high.
+
+
+console.log("Saif")
+
+let name = "saif"  // string
+let age = 20        // numbers value
+let isLoggedIn =false   // boolean types
+
+
+//number =>2 to power 53
+//bigint
+// string =. which will store in both in double quotes "",as well as single quotes''
+// boolean =>   true/false
+//null => standalone value
+// undefined => not assign value till now
+//symbol => unique ke liye
+
+
+//object
+
+console.log(typeof undefined);  //undefined
+
+console.log(typeof null); //object
